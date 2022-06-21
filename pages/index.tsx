1a3a2f350c6f17c5b88de06fc4e4cd7e1ffd0c86
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-06-21 14:58:58 -0300

    grid lines and logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,35 @@ import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <main>
       <Head>
         <title>jamstack 101</title>
         <meta name="description" content="Tailwind is awesome!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-white pb-[40rem]">
-        <h1 className="font-black text-ultra sm:text-9xl text-dark-gray">
+      <section className="bg-white pb-[40rem]">
+        <h1 className="font-black text-3xl sm:text-ultra text-dark-gray">
           jamstack <span className="text-transparent bg-cover bg-no-repeat bg-clip-text bg-pattern">101</span>
         </h1>
         <p className="font-light text-lg text-stone-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.</p>
         <img src="mouse-line.png" title="mouse line" />
-      </main>
-      <article className="max-w-[1397px] m-auto bg-pattern bg-fixed rounded-[3rem] pt-28 relative perspective flex h-[893px] bg-[position:-567px_-579px] items-center flex-col top-[-40rem]">
-        <h1 className="font-black text-ultra sm:text-9xl text-white">javascript</h1>
+      </section>
+      <article className="max-w-[1397px] m-auto bg-pattern bg-fixed rounded-[3rem] pt-28 relative perspective flex h-[893px] bg-[position:-567px_-579px] items-center flex-col mt-[-40rem]">
+        <h2 className="font-black text-3xl sm:text-ultra text-white">javascript</h2>
         <p className="text-white font-light text-lg max-w-xl mt-12 text-center">Statically generated pages are brought to life with client-side JavaScript libraries and frameworks, such as Algolia and Next.js.</p>
         <img src="retro-computer.png" className="absolute top-[28rem]" title="blip bop" />
         <img src="squiggly-arrow.png" className="absolute right-24 bottom-36" title="squiggly arrow" />
+      </article>
+      <img src="falling-logos.svg" className="m-auto -mt-48" />
+      <article>
+        <h2 className="font-black text-3xl sm:text-ultra text-white text-center">api</h2>
+        <p className="text-2xl text-white">At build time, a Jamstack website uses data from one or more APIs to generate markup. These APIs can be a headless CMS like Prismic, a database like Supabase, or even a local JSON file!</p>
       </article>
       <footer className="flex flex-col items-center text-white">
         This is a design test by Thiago Cruz Santos for Monogram.
         <span>❤️</span>
       </footer>
-    </>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,14 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        'ultra': '133px',
+        'ultra': ['133px', '152px'],
         'lg': ['18px', '35px']
       },
       colors: {
         'dark-gray': '#151515'
       },
       backgroundImage: {
-        'pattern': "url('/image.png')",
-        '101': "url('/101.png')"
+        'pattern': "url('/image.png')"
       }
     },
   },
