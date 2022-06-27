fa2305950d2d13c846d58900ab5aa4dcbe0ce088
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-06-27 15:05:05 -0300

    new mobile hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,19 +9,22 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <section id="hero">
-      <div className="flex">
-        <div className="flex-grow">
-          <h1>
-            jamstack <span>101</span>
-          </h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa,
-            tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel
-            pretium tempor ut dui vivamus sit.</p>
-          <span id="mouse-arrow"><Image src={mouse} priority alt="mouse arrow" /></span>
-        </div>
-        <span id="sliding-cards"><Image src={HeroGraphic} alt="sliding cards" /></span>
+    <section id="hero" className="relative">
+      <span id="mouse-arrow">
+        <Image src={mouse} layout="responsive" alt="mouse arrow" />
+      </span>
+      <div>
+        <h1>
+          jamstack <span>101</span>
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa,
+          tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel
+          pretium tempor ut dui vivamus sit.</p>
+        <span id="sliding-cards">
+          <Image width={764} height={964} src={HeroGraphic} alt="sliding cards" />
+        </span>
       </div>
+
     </section>
   )
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   images: {
-    deviceSizes: [340, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [340, 640, 750, 828, 1080, 1200, 1920],
     minimumCacheTTL: 31536000,
   },
   images: {
