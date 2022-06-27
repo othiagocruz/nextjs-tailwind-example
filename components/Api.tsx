a500ea0c7d2257b040e8e0a6fc6cb4cf843b5cd9
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-06-27 09:42:09 -0300

    switching to suspense

--- components/Api.tsx
+++ components/Api.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
 
 
 /**
@@ -8,8 +9,7 @@ import dynamic from 'next/dynamic'
 
 
 const Stage = dynamic(() => import('../components/Stage'), {
-  loading: () => <span>Loading...</span>,
-  ssr: false
+  suspense: true
 })
 
 export default function Api() {
@@ -17,7 +17,9 @@ export default function Api() {
     <article className="text-center m-auto mb-32 md:mb-96 relative">
       <h2 className="font-black mb-12 text-5xl sm:text-ultra text-white text-center">api</h2>
       <p className="mb-5 sm:text-2xl max-w-[730px] m-auto text-white">At build time, a Jamstack website uses data from one or more APIs to generate markup. These APIs can be a headless CMS like Prismic, a database like Supabase, or even a local JSON file!</p>
-      <Stage />
+      <Suspense fallback={`Loading...`}>
+        <Stage />
+      </Suspense>
     </article>
   )
 }
