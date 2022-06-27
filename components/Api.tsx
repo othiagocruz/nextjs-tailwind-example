d33a4ed4b91775f219800b593f7a8703be8de67b
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-06-27 19:19:17 -0300

    trying to improve LCP

--- components/Api.tsx
+++ components/Api.tsx
@@ -1,25 +1,30 @@
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import { useState, useEffect, Suspense, lazy } from 'react'
 
 /**
  * stage sparkle will clip in certain conditions,
  * fix would be to put the entire component on fiber
  */
 
-const Stage = dynamic(() => import('../components/Stage'), {
-  suspense: true
-})
+const Stage = lazy(() => import('../components/Stage'));
 
 export default function Api() {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   return (
     <article id="api">
       <h2>api</h2>
       <p>At build time, a Jamstack website uses data from one or more APIs to generate markup.
         These APIs can be a headless CMS like Prismic, a database like Supabase, or even a
         local JSON file!</p>
-      <Suspense fallback={`Loading...`}>
-        <Stage />
-      </Suspense>
+      {!isMounted ? null : (
+        <Suspense fallback={null}>
+          <Stage />
+        </Suspense>
+      )}
+
     </article>
   )
 }
